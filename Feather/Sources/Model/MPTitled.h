0c2d5f84669c9fb8b11c3171e645b03c7f4dbb01
Matias Piipari <matias.piipari@gmail.com>
2013-04-24 08:51:02 +0100

    Added MPVirtualSection; Cleaned up some copyright claims.

---
+++ Feather/Sources/Model/MPTitled.h
@@ -0,0 +1,14 @@
+//
+//  MPTitled.h
+//  Manuscripts
+//
+//  Created by Matias Piipari on 14/04/2013.
+//  Copyright (c) 2013 Matias Piipari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MPTitled <NSObject>
+@property (readwrite, copy) NSString *title;
+@property (readwrite, copy) NSString *desc;
+@end
--- FeatherTests/MPExtensionTests.m
+++ FeatherTests/MPExtensionTests.m
@@ -3,7 +3,7 @@
 //  Manuscripts
 //
 //  Created by Matias Piipari on 13/02/2013.
-//  Copyright (c) 2013 Manuscripts.app Limited. All rights reserved.
+//  Copyright (c) 2013 Matias Piipari. All rights reserved.
 //
 
 #import "MPExtensionTests.h"
