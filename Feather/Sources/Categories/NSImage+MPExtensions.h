aa5d5d9db32d547b2c44df283500672cbb495ae8
mz2 <matias.piipari@gmail.com>
2014-04-13 18:18:01 +0100

    Adds NSImage+MPExtensions: rounded images.

---
+++ Feather/Sources/Categories/NSImage+MPExtensions.h
@@ -0,0 +1,15 @@
+//
+//  NSImage+MPExtensions.h
+//  Feather
+//
+//  Created by Matias Piipari on 13/04/2014.
+//  Copyright (c) 2014 Matias Piipari. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSImage (MPExtensions)
+
+- (NSImage *)roundCornersImageCornerRadius:(NSInteger)radius;
+
+@end
