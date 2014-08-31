999d1aca3f61a4e8c43bb740de6b7b115b6c3f74
mz2 <matias.piipari@gmail.com>
2014-08-31 14:31:22 +0100

    Special case 'usrf' Apple Event code as it can be used to contain records with text typed keys.

--- Feather/Sources/Categories/NSDictionary+MPExtensions.m
+++ Feather/Sources/Categories/NSDictionary+MPExtensions.m
@@ -91,10 +91,35 @@
     for (NSUInteger i = 0; i < [inDesc numberOfItems]; i++) {
         AEKeyword childDescKey = [inDesc keywordForDescriptorAtIndex:i + 1];
         
-        NSString *propertyName = [[MPScriptingDefinitionManager sharedInstance] cocoaPropertyNameForCode:childDescKey];
-        
-        NSAppleEventDescriptor *childDesc = [inDesc descriptorAtIndex:i + 1];
-        d[propertyName] = childDesc.stringValue;
+        // usrf key means a dictionary with text type keys (at least)
+        if ([[NSString stringWithOSType:childDescKey] isEqualToString:@"usrf"]) {
+            NSAppleEventDescriptor *vals = [inDesc descriptorForKeyword:childDescKey];
+            
+            NSString *name = nil;
+            NSString *value = nil;
+            
+            // 1-indexed, just to be different!
+            NSInteger i = 1;
+            NSInteger count = vals.numberOfItems;
+            for ( ; i <= count; i++) {
+                NSAppleEventDescriptor *desc = [vals descriptorAtIndex:i];
+                
+                NSString *s = [desc stringValue];
+                if (name) {
+                    value = s;
+                    d[name] = value;
+                    name = nil;
+                    value = nil;
+                } else {
+                    name = s;
+                }
+            }
+        } else {
+            NSString *propertyName = [[MPScriptingDefinitionManager sharedInstance] cocoaPropertyNameForCode:childDescKey];
+            
+            NSAppleEventDescriptor *childDesc = [inDesc descriptorAtIndex:i + 1];
+            d[propertyName] = childDesc.stringValue;
+        }
     }
     
     return [d copy];
