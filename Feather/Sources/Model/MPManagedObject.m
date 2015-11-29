2ef335281e21e1a5f55ae9fee403b7452e7e7a07
mz2 <matias.piipari@gmail.com>
2015-11-29 12:24:25 +0000

    Replacing asserts with more NSAsserts / NSParameterAsserts

--- Feather/Sources/Model/MPManagedObject.m
+++ Feather/Sources/Model/MPManagedObject.m
@@ -1620,8 +1620,8 @@ NS_INLINE BOOL isEffectiveGetter(const char* name) {
 
 + (instancetype)modelForDocument:(CBLDocument *)document
 {
-    assert(document);
-    assert(document.database);
+    NSParameterAssert(document);
+    NSParameterAssert(document.database);
     
     if (document.modelObject)
     {
@@ -1637,8 +1637,8 @@ NS_INLINE BOOL isEffectiveGetter(const char* name) {
     if (!mo.controller)
         [mo setControllerWithDocument:document];
     
-    assert(mo.controller);
-    assert([document.properties[@"objectType"] isEqualToString:NSStringFromClass(self)]);
+    NSParameterAssert(mo.controller);
+    NSParameterAssert([document.properties[@"objectType"] isEqualToString:NSStringFromClass(self)]);
     
     return mo;
 }
