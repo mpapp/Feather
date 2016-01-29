90040222ce386a69e0051d6c538d6eb6a60ccf9c
mz2 <matias.piipari@gmail.com>
2016-01-29 09:48:54 -0800

    Adds an extension method to NSString to help clean bad XML String data (XML string that contains two or more ?xml declarations).

---
+++ Feather/NSString+MPExtensions.swift
@@ -0,0 +1,20 @@
+//
+//  NSString+MPExtensions.swift
+//  Feather
+//
+//  Created by Matias Piipari on 29/01/2016.
+//  Copyright © 2016 Matias Piipari. All rights reserved.
+//
+
+import Foundation
+
+public extension NSString {
+    public func XMLStringByRemovingDuplicateXMLDeclarations() -> String {
+        // the preceding character is included in the pattern, therefore captured and included in output.
+        // the pattern below matches to <?xml…?> that is not at the very beginning of the document.
+        let regex = try! NSRegularExpression(pattern:"(.)<\\?xml.*\\?>", options:NSRegularExpressionOptions.CaseInsensitive)
+        let modifiedString = regex.stringByReplacingMatchesInString(self as String, options:[], range:NSRange(location: 0, length: self.length), withTemplate:"$1")
+        
+        return modifiedString
+    }
+}
