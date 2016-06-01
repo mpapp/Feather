97b2949beefc3a4b45ba6f63e2bd94819740696d
Matias Piipari <matias.piipari@gmail.com>
2016-06-02 00:04:34 +0100

    Adds link relation header string parsing to Swift string extensions.

--- FeatherExtensions/String+Extensions.swift
+++ FeatherExtensions/String+Extensions.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import RegexKitLite
 
 public extension String {
     public func stringAroundOccurrence(ofString str:String, maxPadding:UInt, options:NSStringCompareOptions = []) -> String? {
@@ -17,5 +18,31 @@ public extension String {
         let p = Int(maxPadding)
         let r = range.startIndex.advancedBy(-p, limit: self.startIndex) ..< range.endIndex.advancedBy(p, limit: self.endIndex)
         return self.substringWithRange(r)
+    }
+    
+    public enum LinkRelationParsingError: ErrorType {
+        case EmptyString
+        case UnexpectedSection([String])
+    }
+    
+    public func linkRelations() throws -> [String:String] {
+        if self.isEmpty {
+            throw LinkRelationParsingError.EmptyString
+        }
+        
+        var links = [String:String]()
+        for part in self.componentsSeparatedByString(",") {
+            let section = part.componentsSeparatedByString(";")
+            
+            if section.count != 2 {
+                throw LinkRelationParsingError.UnexpectedSection(section)
+            }
+            
+            let url = (section[0] as NSString).stringByReplacingOccurrencesOfRegex("<(.*)>", withString: "$1")
+            let name = (section[1] as NSString).stringByReplacingOccurrencesOfRegex("\\s+rel=\"(.*)\"", withString: "$1")
+            links[name] = url
+        }
+        
+        return links
     }
 }
