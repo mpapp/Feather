322fa93dafeb8106130bf048f7bd3f300c61f0d6
Matias Piipari <matias.piipari@gmail.com>
2016-06-12 22:46:58 +0100

    Adds NSAttributedString+MPExtensions.

---
+++ FeatherAppKitExtensions/NSAttributedString+MPExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  NSAttributedString+MPExtensions.swift
+//  Feather
+//
+//  Created by Matias Piipari on 12/06/2016.
+//  Copyright © 2016 Matias Piipari. All rights reserved.
+//
+
+import Foundation
+
+public extension NSAttributedString {
+    func components(separatedBy string:String) -> [NSAttributedString] {
+        let strRep = self.string
+
+        let separatedArray = strRep.componentsSeparatedByString(string)
+        
+        var start = 0
+        return separatedArray.map { sub in
+            let range = NSMakeRange(start, (sub as NSString).length)
+            let str = self.attributedSubstringFromRange(range)
+            start += range.length + (string as NSString).length
+            
+            return str
+        }
+    }
+}
