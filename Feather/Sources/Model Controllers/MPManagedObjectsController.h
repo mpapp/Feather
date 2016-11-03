ccc5f7e734a62ed51d05aed71593d92106a954f1
Matias Piipari <matias.piipari@gmail.com>
2016-11-03 15:47:16 +0000

    Oops, correction to nullability annotation.

--- Feather/Sources/Model Controllers/MPManagedObjectsController.h
+++ Feather/Sources/Model Controllers/MPManagedObjectsController.h
@@ -188,7 +188,7 @@ typedef enum MPManagedObjectsControllerErrorCode
 @property (readonly, strong, nonnull) NSArray<T> *allObjects;
 
 /** All objects managed by this controller, queried without wrapping to mp_dispatch_sync, and allowing for an error pointer. */
-- (nonnull NSArray<T> *)allObjects:(NSError *__nonnull *__nonnull)error;
+- (nullable NSArray<T> *)allObjects:(NSError *__nullable *__nullable)error;
 
 @property (readonly, strong, nonnull) NSArray<T> *objects;
 
