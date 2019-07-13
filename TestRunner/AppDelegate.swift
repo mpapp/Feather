8b00afdf9e057000a8f1b919aff776111c51eaf6
Matias Piipari <matias.piipari@gmail.com>
2019-07-13 11:35:05 +0300

    Adds TestRunner target.

---
+++ TestRunner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestRunner
+//
+//  Created by Matias Piipari on 13/07/2019.
+//  Copyright © 2019 Matias Piipari. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestRunner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestRunner
+//
+//  Created by Matias Piipari on 13/07/2019.
+//  Copyright © 2019 Matias Piipari. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
